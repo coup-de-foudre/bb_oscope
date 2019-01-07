8a5d5bff29d57551c697feb90d12f4fce19c2d6a
meawoppl <meawoppl@gmail.com>
2019-01-06 20:12:43 -0800

    Finish renaming

--- oscope/trace.py
+++ oscope/trace.py
@@ -3,13 +3,13 @@ import json
 import numpy as np
 
 import oscope.schema
-import oscope.support
+import oscope.array_encoding
 
 
 def package_trace_data(metadata: dict, trace: np.ndarray) -> tuple:
     oscope.schema.validate_message_metadata(metadata)
     encoded_header = json.dumps(metadata).encode("utf-8")
-    encoded_array = oscope.support.ndarray_to_bytes(trace) 
+    encoded_array = oscope.array_encoding.ndarray_to_bytes(trace) 
     return (encoded_header, encoded_array)
 
 
@@ -20,5 +20,5 @@ def unpackage_trace_data(frame: tuple) -> (dict, np.ndarray):
     decoded_header = json.loads(encoded_header.decode())
     oscope.schema.validate_message_metadata(decoded_header)
 
-    decoded_array = oscope.support.bytes_to_ndarray(encoded_array)
+    decoded_array = oscope.array_encoding.bytes_to_ndarray(encoded_array)
     return decoded_header, decoded_array
