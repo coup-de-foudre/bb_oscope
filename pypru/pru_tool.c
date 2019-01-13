7fe029c272112f47a78783a3c3310d5c8ee21e35
meawoppl <meawoppl@gmail.com>
2019-01-12 16:36:22 -0800

    Try that

--- pypru/pru_tool.c
+++ pypru/pru_tool.c
@@ -6,6 +6,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
 
@@ -23,46 +25,27 @@ unsigned int readFileValue(char filename[]){
 
 int main (int argc, char *argv[])
 {
-   if(getuid()!=0){
-      printf("You must run this program as root. Exiting.\n");
-      exit(EXIT_FAILURE);
-   }
+  tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
 
-    if(argc == 0){
-        printf("USAGE\n");
-    }
+  if(getuid() != 0){
+    printf("You must run this program as root. Exiting.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if( argc == 0 ){
+    printf("USAGE\n");
+    exit(EXIT_FAILURE);
+  }
  
-   // Initialize structure used by prussdrv_pruintc_intc
-   // PRUSS_INTC_INITDATA is found in pruss_intc_mapping.h
-   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+  prussdrv_init();
 
-   // Read in the location and address of the shared memory. This value changes
-   // each time a new block of memory is allocated.
-   unsigned int values[2];
-   values[0] = readFileValue(MMAP_LOC "addr");
-   values[1] = readFileValue(MMAP_LOC "size");
-   printf("The shared memory has location: %x and size %x\n", values[0], values[1]);
+  if( strcmp(argv[0], "load") == 0 ) {
+    prussdrv_exec_program (PRU_NUM, argv[1]);
+  } 
+  else if ( strcmp(argv[0], "stop") == 0) {
+    prussdrv_pru_disable(PRU_NUM);
+  }
 
-   // Allocate and initialize memory
-   prussdrv_init ();
-   prussdrv_open (PRU_EVTOUT_0);
-
-   // Write the address and size into PRU0 Data RAM0. You can edit the value to
-   // PRUSS0_PRU1_DATARAM if you wish to write to PRU1
-   prussdrv_pru_write_memory(PRUSS0_PRU0_DATARAM, 0, values, 8);
-
-   // Map PRU's interrupts
-   prussdrv_pruintc_init(&pruss_intc_initdata);
-
-   // Load and execute the PRU program on the PRU
-   prussdrv_exec_program (PRU_NUM, "./memExample.bin");
-
-   // Wait for event completion from PRU, returns the PRU_EVTOUT_0 number
-   int n = prussdrv_pru_wait_event (PRU_EVTOUT_0);
-   printf("EBB PRU program completed, event number %d.\n", n);
-
-   // Disable PRU and close memory mappings
-   prussdrv_pru_disable(PRU_NUM);
-   prussdrv_exit ();
-   return EXIT_SUCCESS;
+  prussdrv_exit();
+  return EXIT_SUCCESS;
 }
