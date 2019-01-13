813d347c7effcb3c038c6d0bd5939921e21f1d37
meawoppl <meawoppl@gmail.com>
2019-01-12 17:21:07 -0800

    Progress

--- pypru/driver.py
+++ pypru/driver.py
@@ -17,8 +17,24 @@ class LibraryLoader:
         with open(CDEF_FILE_PATH) as f:
             self.ffi.cdef(f.read())
 
-        self.lib = self.ffi.dlopen("prussdrv")  
+        self.lib = self.ffi.dlopen("prussdrv")
+
+    def handle_error(self, return_code, callname):
+
+    def __getattr__(self, callname):
+        if hasattr(self.lib):
+            raise AttributeError()
+        
+        unwrapped_call = getattr(self.lib, callname)
+        def wrapper(*args, handle=True):
+            returncode = unwrapped_call(*args)
+            if (returncode != 0) and (returncode is not None) and handle:
+                raise RuntimeError("Call {}({}) returned {}".format(callname, args, returncode))
+            return returncode
+
+        return wrapper
+
 
 # Smoketest
 if __name__ == "__main__":
-    LibraryLoader()
+    LibraryLoader().prussdrv_init()
